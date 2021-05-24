5f77e87ed79b7e8204cc7ae676b9217e59643b0c
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-24 15:11:48 +0200

    Initial Commit

---
+++ RotationGesture/RotationGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationGestureApp.swift
+//  RotationGesture
+//
+//  Created by Ndayisenga Jean Claude on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct RotationGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
