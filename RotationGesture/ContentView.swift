7994e3d22f6df0c76ced0ecea1ebb5595800fe0c
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-24 15:39:16 +0200

    Rotation Gesture

--- RotationGesture/ContentView.swift
+++ RotationGesture/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentAngle: Angle = .degrees(0)
+    @State var finalAngle: Angle = .degrees(0)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Click Me and Rotate")
+                    .bold()
+                    .background(Color.red)
+                    .font(.system(size: 32))
+                    .padding()
+                    .rotationEffect(currentAngle + finalAngle)
+                    .gesture(
+                        RotationGesture()
+                            .onChanged {
+                                angle in
+                                currentAngle = angle
+                            }
+                            .onEnded {
+                                angle in
+                                finalAngle = angle
+                                currentAngle = .degrees(0)
+                            }
+                    )
+            }
+            .navigationTitle("Rotation Effect")
+        }
     }
 }
 
